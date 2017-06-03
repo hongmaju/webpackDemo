28a68c6bbd14b9c6d839803727dc0605206ad163
HongMaJu <517412663@qq.com>
2017-06-03 10:52:46 +0800

    webpack的配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports={
-	entry:'./src/script/main.js',//入口文件
+	// __dirname是nodejs里的一个全局变量，它指向的是我们项目的根目录
+	entry:__dirname+'/src/script/main.js',//入口文件
 	output:{
-		path:'./dist/js',//打包后输出目录
+		path:__dirname+'/dist/js',//打包后输出目录
 		filename:'bundle.js'//打包后输出的文件
 	}
 }
